ca4241cd33ca922be6b23815b7737a6dfd8bf2df
pieroplus <gussunoyoyo@hotmail.co.jp>
2018-05-22 20:36:46 +0900

    layout-change

--- app/models/make.rb
+++ app/models/make.rb
@@ -1,5 +1,5 @@
 class Make < ApplicationRecord
   belongs_to :morimori
   has_many :foodstuff_makes
-  has_many :foodstuffs through: :foodstuff_makes
+  has_many :foodstuffs, through: :foodstuff_makes
 end
